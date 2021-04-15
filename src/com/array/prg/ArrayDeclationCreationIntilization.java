2449c3cee2495058d7a9c1b3ea0755ba236a005e
HOME <HOME@192.168.0.4>
2021-04-15 23:20:20 +0530

    comments

---
+++ src/com/array/prg/ArrayDeclationCreationIntilization.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.array.prg;
+
+/**
+ *
+ * @author HOME sdsdas
+ */
+public class ArrayDeclationCreationIntilization {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        int[] a=new int[1];
+//        int a[]=new int[1];
+//        int []a=new int[1];
+//        int[][] a= new int[10][20];
+//        int[][] a= new int[60][20];
+        int[] a= new int[2];
+        System.out.println("a : "+a.length);
+    }
+    
+}
